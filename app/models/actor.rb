8ef4be731c3c29bc722c0d99907f64cb28487642
github username <github email address>
2020-03-19 20:33:33 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,20 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name 
+        # binding.pry
+        self.first_name + " " + self.last_name
+    end 
+
+    def list_roles 
+        
+        show = self.shows.map {|s| s.name}
+        character = self.characters.map{|c| c.name}
+        
+        character_and_shows = character << " - " << show
+        character_and_shows.flatten!
+        character_and_shows.join()
+        
+    end 
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,16 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+    def actors_list
+        # binding.pry
+        @cast = []
+        @cast = self.actors.map do |actor|
+            full_name = actor.first_name + " " + actor.last_name
+            @cast<<full_name.to_s
+        end 
+        @cast.flatten
+    end 
+
 end
